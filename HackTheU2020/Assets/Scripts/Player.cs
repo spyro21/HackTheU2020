e061bf6e0449912f36d4e680f9936f6c626b09c5
spyro21 <38793743+spyro21@users.noreply.github.com>
2020-10-16 17:08:46 -0600

    Title Screen & rotation start
    -made title screen start
    -learned scene management
    -added rotation to player, but want to improve by making it momentum based.

---
+++ HackTheU2020/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float rotationZ;
+    public float rotationSpeed;
+    public bool clockWiseRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rotationZ += Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime;
+
+
+        transform.rotation = Quaternion.Euler(0, 0, rotationZ);
+    }
+}
